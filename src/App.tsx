8b9f736496604a903dc510df231eefb7e24c7eae
Marcos Fraga <marcosjsfraga@gmail.com>
2021-03-17 22:35:25 -0300

    Many changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
 import { Dashboard } from "./component/Dashboard";
 import { Header } from "./component/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
-  return (
-      <>
-          <Header />
-          <Dashboard />
-          <GlobalStyle />
-      </>
-  );
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+    function openNewTransactionModalHandler() {
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function closeNewTransactionModalHandler() {
+        setIsNewTransactionModalOpen(false)
+    }
+
+    return (
+        <>
+            <Header onOpenNewTransactionModal={openNewTransactionModalHandler} />
+
+            <Dashboard />
+
+            <Modal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={closeNewTransactionModalHandler}
+                >
+                <h2>Cadastrar transação</h2>
+            </Modal>
+
+            <GlobalStyle />
+        </>
+    );
 }
--- src/component/TransactionsTable/styles.ts
+++ src/component/TransactionsTable/styles.ts
@@ -5,18 +5,19 @@ export const Container = styled.header`
 
     table {
         width: 100%;
-        border-spacing: 0 0.5rem;
+        border-spacing: 0 0.3rem;
 
         th {
             color: var(--text-body);
-            font-weight: 400;
-            padding: 1rem 2rem;
+            font-weight: 600;
+            font-size: 1.1rem;
+            padding: 0.5rem 2rem;
             text-align: left;
-            line-height: 1.5rem;
+            line-height: 0.5rem;
         }
 
         td {
-            padding: 1rem 2rem;
+            padding: 0.8rem 1rem;
             border: 0;
             background: var(--shape);
             color: var(--text-body);
